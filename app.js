2bc76a7dec495244b2719882a23ca0e875281177
dvelezroman <dvelezroman@gmail.com>
2018-05-16 11:17:53 -0300

    actualizado los app.use()

--- app.js
+++ app.js
@@ -2,38 +2,58 @@ const express = require('express');
 const app = express(); // crea una instancia de una aplicacion de express
 const nunjucks = require('nunjucks');
 
+app.set('view engine', 'html'); // hace que res.render funcione con archivos html
+app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
+
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED');
+  next();
+};
+
+app.use(myLogger);
+app.use(requestTime);
+
+app.get('/', function (req, res, next) {
+  var responseText = 'Hello World!';
+  responseText += 'Requested at: ' + req.requestTime + '';
+  //res.send(responseText);
+  console.log(responseText);
+  next();
+});
+
 app.get('/', function(req, res){
+	res.send('Segundo app.get()');
+})
+
+app.get('/halloffame', function(req, res){
 	const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 	res.render( 'index', {title: 'Hall of Fame', people: people} );
-	//res.send('Hola');
 });
-app.listen(3000);
-
+// GET method route
 app.use('/', function(req, res, next ){
 	console.log('Hicieron un Request a '+ req.url);
 	next();
 });
 
-app.use('/especial/subpath', function(req, res, next){
-	console.log('Haz llegado a un area especial...');
-	next();
+app.post('/', function (req, res, next) {
+  	console.log('POST request to the homepage');
+  	next();
 });
 
-app.set('view engine', 'html'); // hace que res.render funcione con archivos html
-app.engine('html', nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
-nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
+app.all('/secret', function (req, res, next) {
+  	console.log('Accessing the secret section ...');
+  	next(); // pass control to the next handler
+});
 
-// var locals = {
-//     title: 'An Example',
-//     people: [
-//         { name: 'Gandalf'},
-//         { name: 'Frodo' },
-//         { name: 'Hermione'}
-//     ]
-// };
-// nunjucks.configure('views', {noCache: true});
-// nunjucks.render('index.html', locals, function (err, output) {
-//     console.log(output);
-// });
+app.use('/especial/subpath', function(req, res, next){
+	console.log('Haz llegado a un area especial...');
+	
+});
 
-
+app.listen(3000);
